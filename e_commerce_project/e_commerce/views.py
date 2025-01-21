b0a6e1fd8abb9fd4d2276a1bf990c2fdc677fb2f
Naira <naira.awadin@epitech.eu>
2025-01-21 16:25:20 +0100

    [ADD] model, views, templates - nom, description et image

--- e_commerce_project/e_commerce/views.py
+++ e_commerce_project/e_commerce/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import product
 
-# Create your views here.
+# Create your views here
+def product_list(request):
+    products = product.objects.all().order_by('-created_at').all()
+    return render(request, 'e_commerce/product_list.html', {'products': products})
