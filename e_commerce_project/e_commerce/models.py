03fecac1ab97f00b006da6e80e07b88eef0bf88b
Naira <naira.awadin@epitech.eu>
2025-01-21 16:46:40 +0100

    [SUPP] L'image dynamique pour revenir à une image static

--- e_commerce_project/e_commerce/models.py
+++ e_commerce_project/e_commerce/models.py
@@ -4,7 +4,7 @@ class product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-    img = models.ImageField(upload_to ='images/', null=True)
+    ##img = models.ImageField(upload_to ='images/', null=True)
 
     def __str__(self):
         return self.name
