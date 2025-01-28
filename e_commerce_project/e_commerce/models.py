6f5c11a976630649cf3c225d8a85f714bc7491b2
Naira <naira.awadin@epitech.eu>
2025-01-28 22:32:16 +0100

    [ADD] Images dynamiques, le champs price, la redirection vers la page de description quand on clique sur un produit et enfin le CSS de la page de listing et la page de description.

--- e_commerce_project/e_commerce/models.py
+++ e_commerce_project/e_commerce/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-    ##img = models.ImageField(upload_to ='images/', null=True)
+    img = models.ImageField(upload_to='images/', null=True)
 
     def __str__(self):
         return self.name
--- e_commerce_project/e_commerce/views.py
+++ e_commerce_project/e_commerce/views.py
@@ -6,3 +6,7 @@ from .models import product
 def product_list(request):
     products = product.objects.all().order_by('-created_at').all()
     return render(request, 'e_commerce/product_list.html', {'products': products})
+
+def product_details(request, id):
+    details = product.objects.get(id=id)
+    return render(request, 'e_commerce/product_details.html', {'details': details})
